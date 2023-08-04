a0ef54799caa43aeae75980cbd2ccf1a157e5601
Piotr Stawarski <piotr.stawarski@zdt.io>
2023-08-04 15:44:52 +0200

    locks

---
+++ backend-dynamodb.tf
@@ -0,0 +1,28 @@
+## DynamoDB Table for state locking.
+
+resource "aws_dynamodb_table" "state" {
+  name         = var.dynamodb_table_name
+  tags         = var.dynamodb_table_tags
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+}
+
+
+data "aws_iam_policy_document" "dynamodb" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:DescribeTable",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem",
+    ]
+    resources = [aws_dynamodb_table.state.arn]
+  }
+}
